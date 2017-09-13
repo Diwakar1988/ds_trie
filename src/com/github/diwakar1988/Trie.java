963b385d034bc2690d55259eceae1c546998f919
Diwakar Mishra <diwakar.mishra@equinox.com>
2017-09-13 13:13:22 +0530

    trie node and helper method added

--- src/com/github/diwakar1988/Trie.java
+++ src/com/github/diwakar1988/Trie.java
@@ -2,4 +2,16 @@ package com.github.diwakar1988;
 
 public class Trie {
 
+	private Node root;
+	
+	public void add(String str) {
+		
+	}
+	public void remove(String str) {
+		
+	}
+	public boolean search(String prefix) {
+		return false;
+	}
+
 }
