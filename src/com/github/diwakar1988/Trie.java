d0bd594fcec81c9042dc3ae16773a627eeff842b
Diwakar Mishra <diwakar.mishra@equinox.com>
2017-09-13 13:40:06 +0530

    insert method added

--- src/com/github/diwakar1988/Trie.java
+++ src/com/github/diwakar1988/Trie.java
@@ -1,11 +1,54 @@
 package com.github.diwakar1988;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Trie {
 
+	private class Node {
+
+		private Map<Character, Node> childs=new HashMap<>();
+		private boolean endOfWord;
+		
+		public Node setChild(char ch) {
+			Node n = new Node();
+			this.childs.put(ch, n);
+			return n;
+		}
+		public Node setChild(char ch,boolean endOfWord) {
+			Node n = setChild(ch);
+			if (endOfWord) {			
+				n.markEndOfWord();
+			}
+			return n;
+		}
+		public Node getChild(char ch) {
+			return childs.get(ch);
+		}
+		public boolean isEndOfWord() {
+			return endOfWord;
+		}
+		public void markEndOfWord() {
+			this.endOfWord = true;
+		}
+
+	}
 	private Node root;
 	
-	public void add(String str) {
-		
+	public Trie() {
+		root = new Node();
+	}
+	public void insert(String str) {
+		Node current=root;
+		for(int i=0;i<str.length();i++){
+			char ch = str.charAt(i);
+			Node child = current.getChild(ch);
+			if (child==null) {
+				child = current.setChild(ch);
+			}
+			current=child;
+		}
+		current.markEndOfWord();
 	}
 	public void remove(String str) {
 		
