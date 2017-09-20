6d0dbedd5c391b5f84fe0ac660300dfdd777f8fb
Diwakar Mishra <diwakar.mishra@equinox.com>
2017-09-20 20:20:43 +0530

    test cases updated

--- src/com/github/diwakar1988/Trie.java
+++ src/com/github/diwakar1988/Trie.java
@@ -1,17 +1,23 @@
 package com.github.diwakar1988;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 
 public class Trie {
 
 	private class Node {
-
+		private char data;
 		private Map<Character, Node> childs=new HashMap<>();
 		private boolean endOfWord;
-		
+		public Node(char data) {
+			this.data=data;
+		}
 		public Node setChild(char ch) {
-			Node n = new Node();
+			Node n = new Node(ch);
 			this.childs.put(ch, n);
 			return n;
 		}
@@ -36,7 +42,7 @@ public class Trie {
 	private Node root;
 	
 	public Trie() {
-		root = new Node();
+		root = new Node(Character.MIN_VALUE);
 	}
 	public void insert(String str) {
 		Node current=root;
@@ -53,8 +59,61 @@ public class Trie {
 	public void remove(String str) {
 		
 	}
-	public boolean search(String prefix) {
-		return false;
+	public List<String> search(String prefix) {
+		List<String> list = new ArrayList<>();
+		Node current=root;
+		for(int i=0;i<prefix.length();i++){
+			char ch = prefix.charAt(i);
+			Node child = current.getChild(ch);
+			if (child==null) {
+				//prefix not found here
+				return list;
+			}
+			current=child;
+		}
+		addChars(prefix,current,list,new StringBuilder(prefix));
+		return list;
 	}
+	private void addChars(String prefix, Node current, List<String> list,StringBuilder sb) {
+		sb.append(current.data);
+		if (current.endOfWord) {
+			list.add(sb.toString());
+			sb.setLength(0);
+			sb.append(prefix);
+			return;
+		}
+		Iterator<Character> chars = current.childs.keySet().iterator();
+		while (chars.hasNext()) {
+			char ch = (char) chars.next();
+			Node leaf = current.getChild(ch);
+			addChars(prefix, leaf, list,sb);
+		}
+		
+	}
+	public boolean searchPrefix(String prefix) {
+		Node current=root;
+		for(int i=0;i<prefix.length();i++){
+			char ch = prefix.charAt(i);
+			Node child = current.getChild(ch);
+			if (child==null) {
+				return false;
+			}
+			current=child;
+		}
+		return true;
+	}
+	public boolean searchWord(String word) {
+		Node current=root;
+		for(int i=0;i<word.length();i++){
+			char ch = word.charAt(i);
+			Node child = current.getChild(ch);
+			if (child==null) {
+				return false;
+			}
+			current=child;
+		}
+		return current.endOfWord;
+	}
+	
 
 }
--- test/com/github/diwakar1988/TrieTest.java
+++ test/com/github/diwakar1988/TrieTest.java
@@ -2,13 +2,48 @@ package com.github.diwakar1988;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class TrieTest {
 
+	private Trie trie;
+	@Before
+	public void setup(){
+		trie = new Trie();
+		trie.insert("diwakar");
+		trie.insert("dinesh");
+		trie.insert("dinkar");
+		trie.insert("dipankar");
+		trie.insert("devesh");
+		trie.insert("danish");
+		trie.insert("rani");
+		
+	}
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSearchPrefix() {
+		assertTrue(trie.searchPrefix("di"));
+		assertTrue(trie.searchPrefix("din"));
+		assertTrue(trie.searchPrefix("ran"));
+		assertTrue(trie.searchPrefix("da"));
+		assertTrue(trie.searchPrefix("de"));
+		assertFalse(trie.searchPrefix("dw"));
+		assertFalse(trie.searchPrefix("in"));
+	}
+	@Test
+	public void testSearchWord() {
+		assertTrue(trie.searchWord("diwakar"));
+		assertFalse(trie.searchWord("din"));
+		assertTrue(trie.searchWord("dinkar"));
+		assertFalse(trie.searchWord("dev"));
+		assertTrue(trie.searchWord("rani"));
+	}
+	@Test
+	public void testSearch() {
+		List<String> list = trie.search("di");
+		System.out.println(list);
 	}
 
 }
